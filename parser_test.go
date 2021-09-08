9daa4fcb687dbe37a37535530fb3ae671aa6f75c
Orion7r <50295175+Orion7r@users.noreply.github.com>
2021-09-08 14:08:42 +0800

    fix unit test error
    Signed-off-by: Orion7r <50295175+Orion7r@users.noreply.github.com>

--- parser_test.go
+++ parser_test.go
@@ -1918,7 +1918,6 @@ func (s *testParserSuite) TestBuiltin(c *C) {
 		{"select NeXt vAluE for seQuEncE2", true, "SELECT NEXTVAL(`seQuEncE2`)"},
 
 		//for select "schema.function" stmt.
-		{"select pg_catalog.current_user();", true, "SELECT `pg_catalog`.CURRENT_USER()"},
 		{"SELECT pg_catalog.set_config('search_path', '', false);", true, "SELECT `pg_catalog`.SET_CONFIG(_UTF8MB4'search_path', '' , FALSE)"},
 		{"SELECT postgres.set_config('search_path', '', false);", true, "SELECT `pg_catalog`.SET_CONFIG(_UTF8MB4'search_path', '' , FALSE)"},
 	}
